91f5dfddd3c831cfa31eb884b5cae9cafa7ea0a9
donk05 <872888810@qq.com>
2026-09-23 17:49:22 +0800

    公共字段自动填充，AOp思想

---
+++ sky-server/src/main/java/com/sky/aspect/AutoFIllAspect.java
@@ -0,0 +1,111 @@
+package com.sky.aspect;
+
+import com.sky.annotation.AutoFill;
+import com.sky.constant.AutoFillConstant;
+import com.sky.context.BaseContext;
+import com.sky.enumeration.OperationType;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.binding.MapperMethod;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+import java.time.LocalDateTime;
+
+/**
+ * 自定义切面，实现公告字段自动填充
+ *
+ */
+@Aspect
+@Component
+@Slf4j
+public class AutoFIllAspect {
+    /**
+     * 切入点
+     */
+
+    @Pointcut("execution(* com.sky.mapper.*.*(..)) && @annotation(com.sky.annotation.AutoFill)")
+
+    public void autoFillPointCut(){
+
+    }
+
+    /**
+     * 前置通知，为公共字段赋值
+     */
+    @Before("autoFillPointCut()")
+    public void autoFill(JoinPoint joinPoint){
+
+        log.info("开始进行字段填充");
+
+        //1.获取拦截方法的数据库操作类型
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        AutoFill autoFill = signature.getMethod().getAnnotation(AutoFill.class);
+        OperationType operationType = autoFill.value();
+
+
+
+        //2.获取方法参数，实体对象
+        Object[] args = joinPoint.getArgs();
+        if(args==null || args.length==0) return;
+        Object arg = args[0];
+        //3。赋值数据，通过反射
+        LocalDateTime now = LocalDateTime.now();
+        Long currentId = BaseContext.getCurrentId();
+        if(operationType==OperationType.INSERT){
+            //为四个公告字段赋值（反射）
+            try {
+                //获得set方法
+                Method setCreateTime = arg.getClass().getDeclaredMethod(AutoFillConstant.SET_CREATE_TIME, LocalDateTime.class);
+                Method setCreateUser = arg.getClass().getDeclaredMethod(AutoFillConstant.SET_CREATE_USER, Long.class);
+                Method setUpdateTime = arg.getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_TIME, LocalDateTime.class);
+                Method setUpdateUser = arg.getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_USER, Long.class);
+
+                //通过反射赋值
+                setCreateTime.invoke(arg,now);
+                setCreateUser.invoke(arg,currentId);
+                setUpdateTime.invoke(arg,now);
+                setUpdateUser.invoke(arg,currentId);
+
+
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+
+        }
+        else if(operationType==OperationType.UPDATE){
+            //为两个公告字段赋值
+            try {
+                //获得set方法
+                //Method setCreateTime = arg.getClass().getDeclaredMethod("setCreateTime", LocalDateTime.class);
+                //Method setCreateUser = arg.getClass().getDeclaredMethod("setCreateUser", Long.class);
+                Method setUpdateTime = arg.getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_TIME, LocalDateTime.class);
+                Method setUpdateUser = arg.getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_USER, Long.class);
+
+                //通过反射赋值
+                //setCreateTime.invoke(arg,now);
+                //setCreateUser.invoke(arg,currentId);
+                setUpdateTime.invoke(arg,now);
+                setUpdateUser.invoke(arg,currentId);
+
+
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+
+
+        }
+
+
+    }
+
+
+
+}
